abaaac3279f88f9b06197ed29d9636114b0169f6
Ben <bigriffin94@outlook.com>
2019-08-22 16:34:37 -0700

    Standup Submit

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
-import { Form, Field, Formik, withFormik } from 'formik';
-
+import React, { useState, useEffect} from 'react';
+import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
 const UserForm = ( { errors, touched, values, status} ) => {
+    const [users, setUsers] = useState([]);
+    console.log("this is touched", touched);
+    useEffect(() =>{
+        if (status){
+            setUsers([...users, status]);
+        }
+     }, [status]);
  return (
     <div className='user-form'>
-        <Formik>
         <Form>
             User Login
-            <Field name='name' type='text' placeholder='Full Name'/>
-            {touched.name && errors.name && <p>{errors.name}</p>}
-            <Field name='email' type='email' placeholder='E-mail Address'/>
+            <Field name='name' 
+            type='text' 
+            placeholder='Full Name'
+            />
+            {touched.name && errors.name && (<p>{errors.name}</p>)}
+
+            <Field 
+            name='email' 
+            type='email' 
+            placeholder='E-mail Address'
+            required
+            />
             {touched.email && errors.email && <p>{errors.email}</p>}
+
             <Field name='password' type='password' id='pass' minLength='5' placeholder='Password (5 characters min)'/>
             {touched.password && errors.password && <p>{errors.password}</p>}
             <label className="check-box">
                 Terms of Service:
-               <Field type='checkbox' name='terms'/>
-               {touched.checkbox && errors.checkbox && <p>{errors.checkbox}</p>} 
+                {touched.terms && errors.terms && <p>{errors.terms}</p>} 
+               <Field type='checkbox' name='terms' checked={values.terms}/>
+              
                <span className="checkmark"/>
             </label>
             <button className='submit-btn' type='submit'>Submit!</button>
         </Form>
-        </Formik>
+        {users.map(user => (
+                <ul key={user.id}>
+                    <li>User Name: {user.name}</li>
+                    <li>Email: {user.email}</li>
+                    <li>Password: {user.password}</li>
+                </ul>
+            ))}
     </div>
  ) //end return 
 } // END Form
@@ -42,15 +64,17 @@ const FormikUserForm = withFormik({
         name: Yup.string().required("Name is required!"),
         email: Yup.string().required("Email Required."),
         password: Yup.string().required("password required."),
-        terms: Yup.string().required("Terms of service required.")
+        terms: Yup.string().required("Terms of service required."),
     }),
-    handleSubmit(values, { setStatus }) {
+    handleSubmit(values, { setSubmitting }) {
         axios.post("https://reqres.in/api/users/", values)
         .then(res => {
             console.log(res.data);
-            setStatus(res.data)}) 
+            setSubmitting(res.data)
+        }) 
         .catch(err => {
             console.log(err.response)
+            // setSubmitting(false)
         })
     }
 })(UserForm);
---
+++ src/Components/Test.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect} from 'react';
+import axios from "axios";
+import { Form, Field, withFormik} from "formik";
+import * as Yup from "yup";
+
+const UserForm = ({errors, touched, values, status}) =>{
+    const [users, setUsers] = useState([]);
+    console.log("this is touched", touched);
+    useEffect(() =>{
+        if (status){
+            setUsers([...users, status]);
+        }
+     }, [status])
+    return(
+        <div className="form-parent">
+            <Form>
+                <label>
+                Name
+                <Field 
+                name="user" 
+                type="text" 
+                placeholder="User Name"
+                />
+                {touched.user && errors.user && (
+                    <p>{errors.user}</p>
+                )}
+                </label>
+                <label>
+                    Email
+                    <Field 
+                    name="email" 
+                    type="email" placeholder="email@email.com" 
+                    required/>
+                </label>  
+                <label>
+                    Password
+                    <Field name="password" type="password" placeholder="password"/>
+                    {touched.password && errors.password && (
+                    <p>{errors.password}</p>
+                )}
+                </label>
+                
+                <label className="checkmark-container">
+                    Terms of Service
+                    <Field
+                        type="checkbox"
+                        name="terms"
+                        checked={values.terms}
+                    />
+                    <span className="checkmark"/>
+                </label>
+                <button>Submit!</button>
+            </Form>
+            {users.map(user => (
+                <ul key={user.id}>
+                    <li>User Name: {user.user}</li>
+                    <li>Email: {user.email}</li>
+                    <li>Password: {user.password}</li>
+                </ul>
+            ))}
+        </div>
+    );
+}
+const FormikUserForm = withFormik({
+    mapPropsToValues({ user, email, password, terms}){
+        return{
+            user: user || "",
+            email: email || "",
+            password: password || "",
+            terms: terms || false
+        };
+    },
+    validateSchema: Yup.object().shape({
+        user: Yup.string().required("required"),
+        password: Yup.string().required("required")
+    }),
+
+    handleSubmit(values, { setStatus }) {
+        axios
+          .post("https://reqres.in/api/users/", values)
+          .then(res => {
+            setStatus(res.data);
+            console.log(values);
+          })
+          .catch(err => console.log(err.response));
+      }
+})(UserForm);
+
+export default FormikUserForm;
